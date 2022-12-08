63e3f437457d005144129c6e19698613db9cea5a
TashMb <natashabwalya65@gmail.com>
2022-12-08 14:34:11 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- * return:Always 0
+ * main - Receives no argument value
+ *
+ * return: Always 0 (success)
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
