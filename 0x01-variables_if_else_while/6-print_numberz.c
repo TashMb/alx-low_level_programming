646508a5b068f87288d50fe83c27f7fb47393423
TashMb <natashabwalya65@gmail.com>
2022-12-11 14:50:55 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -Entry
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int num = '0';
+
+while (num <= 9)
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+return (0);
+}
