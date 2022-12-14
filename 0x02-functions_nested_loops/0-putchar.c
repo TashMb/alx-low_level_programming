27921dd20394473219245f3891ac87404d19f5e5
TashMb <natashabwalya65@gmail.com>
2022-12-14 13:37:55 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry and receives no argument value
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("_putchar\n");
+
+return (0);
+}
