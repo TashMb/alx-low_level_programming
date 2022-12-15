441e8b2361b3c6d4cb8f8a77d3b5066c36a9122f
TashMb <natashabwalya65@gmail.com>
2022-12-15 14:41:43 -0800

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - tests if int is positive or negative
+ * @i: int in question
+ * Return: 0
+ */
+
+
+
+void positive_or_negative(int i)
+
+{
+int i;
+
+i = 98;
+
+positive_or_negative(i);
+
+
+
+return (0);
+
+}
