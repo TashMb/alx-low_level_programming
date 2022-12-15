a3d3baf4f050b42c8a1d2077f297846af76db193
TashMb <natashabwalya65@gmail.com>
2022-12-14 17:35:30 -0800

    print alphabet ten

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10 - print alphabet ten times
+ */
+void print_alphabet_x10(void)
+{
+
+char letter = 'a';
+
+while (letter <= 'z')
+{
+int count = 0;
+
+while (count++ <= 9)
+
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
