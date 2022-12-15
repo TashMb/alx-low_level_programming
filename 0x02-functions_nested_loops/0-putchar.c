5135572afc60f0b4d4d61db23ab8e6a20b010099
TashMb <natashabwalya65@gmail.com>
2022-12-14 16:14:07 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entry and receives no argument value
+ * main - entry point
+ * Description: program that prints _putchar 
  * Return: Always 0 (success)
  */
 int main(void)
